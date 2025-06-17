b7824fc44fd069cd67a6e5f9cacb9f04c4a70df1
lmangall <l.mangallon@gmail.com>
2025-06-17 19:46:57 +0200

    fix: change satellite ID for the one linked to Domain

--- juno.config.ts
+++ juno.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   satellite: {
     ids: {
       //   development: '<DEV_SATELLITE_ID>',
-      development: "ak7hz-xyaaa-aaaal-asgqq-cai",
+      development: "5yoof-ciaaa-aaaal-asevq-cai",
       //   production: "<PROD_SATELLITE_ID>",
-      production: "ak7hz-xyaaa-aaaal-asgqq-cai",
+      production: "5yoof-ciaaa-aaaal-asevq-cai",
     },
     source: "out",
     predeploy: ["npm run build"]
